079af2b4699c30fc74ad50bd70da042d4076374e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-06 18:57:05 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,11 +26,11 @@ class InteractiveRecord
     end
 
     def table_name_for_insert
-
+      self.class.table_name
     end
 
     def col_names_for_insert
-
+      self.class.column_names
     end
 
     def values_for_insert
