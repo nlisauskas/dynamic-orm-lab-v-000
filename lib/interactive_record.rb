a3f62637e8bd7bf10f53ba106247f7ed420e5e7a
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-06 19:08:56 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,6 +48,13 @@ class InteractiveRecord
     end
 
     def self.find_by_name(name)
+      sql = <<-SQL
+      SELECT *
+      FROM #{self.table_name}
+      WHERE name = #{name}
+      SQL
+
+      DB[:conn].execute(sql)
 
     end
 
